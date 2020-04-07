91b6c4fa7fa682851c29b0bf96b9e2cdd76e31be
KoFeeBriAN <coffeetk2017@gmail.com>
2020-04-07 11:20:25 +0700

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: { app: "./src/index.js" },
+  mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "My First Three App",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
